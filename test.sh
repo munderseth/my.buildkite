07e56cffd6413a546c4974f30f9f7a0b8a79416d
munderseth <marku@s2technologies.com>
2023-08-15 14:34:25 -0700

    use CI setting

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#testspace -v
+#git clone https://github.com/munderseth/my.buildkite.git
+
+export CI=true
+testspace config url @munderseth.testspace.com
+testspace results.xml "#ThisResults"
