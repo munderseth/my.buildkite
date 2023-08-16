60d10193d0a122598b291024a98db1c117b78701
munderseth <marku@s2technologies.com>
2023-08-16 12:58:53 -0700

    use hook, full url

--- test.sh
+++ test.sh
@@ -4,5 +4,6 @@
 #git clone https://github.com/munderseth/my.buildkite.git
 
 export CI=true
-testspace config url @munderseth.testspace.com
-testspace results.xml "#ThisResults"
+#testspace config url @munderseth.testspace.com
+testspace results.xml "munderseth.testspace.com/munderseth:my.buildkite/main#ThisResults"
+
