e9eda1eb6fb35710f650f212c520b41b78efcf25
munderseth <marku@s2technologies.com>
2023-08-18 13:56:22 -0700

    use token within pipeline (anti-pattern)

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#export CI=true
-testspace config url @munderseth.testspace.com
-#testspace results.xml "munderseth.testspace.com/munderseth:my.buildkite/main#ThisResults"
-
--- trybat.bat
+++
@@ -1,5 +0,0 @@
-echo "Push Results"
-set CI=true
-testspace config url munderseth.testspace.com
-testspace config
-REM testspace results.xml
